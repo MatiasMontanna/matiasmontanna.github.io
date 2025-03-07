fc09e8f5e3f3ceb15f5a7c07a16eb8e1ad93d014
MatiasMontanna <matiasmontanna@hotmail.com>
2025-03-07 15:04:53 -0300

    test

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1741370334',resources: [
+    cacheName: 'chirpy-1741370612',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
